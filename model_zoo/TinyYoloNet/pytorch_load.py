b0018f07b472b934b03140abde598e36878763bf
Unknown <lah2000@naver.com>
2018-03-15 10:39:18 +0900

    fix yolo maxpool

--- model_zoo/TinyYoloNet/pytorch_load.py
+++ model_zoo/TinyYoloNet/pytorch_load.py
@@ -1,71 +1,52 @@
 
-import numpy as np
 import torch.nn as nn
-#import torch.nn.functional as F
-from collections import OrderedDict
-#import sys
-#sys.setrecursionlimit(20000)
 
 class BasicConv2D(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0, bias=False):
+    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0, bias=False, add_maxpool=False, pool_stride=0):
         super(BasicConv2D, self).__init__()
+        self.add_maxpool = add_maxpool
+        self.pool_stride = pool_stride
+
         self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
                               bias=False)  # verify bias false
         self.bn = nn.BatchNorm2d(out_planes)
         self.relu = nn.LeakyReLU(0.1, inplace=True)
+        if self.add_maxpool:
+            if self.pool_stride == 2:
+                self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
+        if self.add_maxpool:
+            if self.pool_stride == 2:
+                x = self.maxpool(x)
+            elif self.pool_stride == 1:
+                x = nn.MaxPool2d(self.pad(x, (0, 1, 0, 1), mode='replicate'), 2, stride=1)
         return x
-
-
-class Maxpool2D(nn.Module):
-    def __init__(self, kernel_size=2, stride=2):
-        super(Maxpool2D, self).__init__()
-
-    def forward(self, x):
-        x = nn.MaxPool2d(kernel_size=2, stride=2)
-        return x
-
-
-class MaxPoolStride1(nn.Module):
-    def __init__(self):
-        super(MaxPoolStride1, self).__init__()
-
-    def forward(self, x):
-        x = nn.MaxPool2d(self.pad(x, (0, 1, 0, 1), mode='replicate'), 2, stride=1)
-        return x
-
 
 class TinyYoloNet(nn.Module):
     def __init__(self, num_output=1000):
         super(TinyYoloNet, self).__init__()
         self.features = nn.Sequential(
             # conv1
-            BasicConv2D(3, 16, 3, 1, 1, bias=False),
-            Maxpool2D(2, 2),
+            BasicConv2D(3, 16, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=2),
 
             # conv2
-            BasicConv2D(16, 32, 3, 1, 1, bias=False),
-            Maxpool2D(2, 2),
+            BasicConv2D(16, 32, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=2),
 
             # conv3
-            BasicConv2D(32, 64, 3, 1, 1, bias=False),
-            Maxpool2D(2, 2),
+            BasicConv2D(32, 64, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=2),
 
             # conv4
-            BasicConv2D(64, 128, 3, 1, 1, bias=False),
-            Maxpool2D(2, 2),
+            BasicConv2D(64, 128, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=2),
 
             # conv5
-            BasicConv2D(128, 256, 3, 1, 1, bias=False),
-            Maxpool2D(2, 2),
+            BasicConv2D(128, 256, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=2),
 
             # conv6
-            BasicConv2D(256, 512, 3, 1, 1, bias=False),
-            MaxPoolStride1(),
+            BasicConv2D(256, 512, 3, 1, 1, bias=False, add_maxpool=True, pool_stride=1),
 
             # conv7
             BasicConv2D(512, 1024, 3, 1, 1, bias=False),
